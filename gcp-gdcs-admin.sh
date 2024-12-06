099538c2cf6584a07a51580121e6b9e202a69e34
shiyghannavti <136981389+shiyghannavti@users.noreply.github.com>
2024-12-06 06:40:35 +0000

    Update gcp-gdcs-admin.sh

--- gcp-gdcs-admin.sh
+++ gcp-gdcs-admin.sh
@@ -66,7 +66,7 @@ cat <<EOF > $PROJDIR/.env
 export GCP_PROJECT=$GCP_PROJECT
 export GCP_REGION=us-west1
 export GCP_ZONE=us-west1-a
-export SERVICEMESH_VERSION=1.22.4-asm.0
+export SERVICEMESH_VERSION=1.23.3-asm.2 # 1.22.4-asm.0
 export ANTHOS_VERSION=1.16.12
 EOF
 source $PROJDIR/.env
